7193247e7994e41b5a42ed15eadada2b9e118773
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2024-11-22 22:41:23 -0800

    Update app.py

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world! This is lover Samsor Rahman'
 
 if __name__ == '__main__':
     app.run()
